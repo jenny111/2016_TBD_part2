68eaccb15ebe79e888cad4b7b67ce9a8e2eccebd
Jenny <jenny@ygui.me>
2017-06-10 21:17:43 +0800

    add basicits.py from sourcetree

---
+++ basicts.py
@@ -0,0 +1,13 @@
+class BasicTimeSeriesAnalysis(object):
+
+
+	def __init__(self,hist=None):
+		self.hist=hist
+		self.prediction=None
+		self.model=None
+
+	def train(self):
+		pass
+
+	def predict(self,length):
+		pass
